66876a08ff892399cb34b875c2cd33e006eba135
wen <1877065005@qq.com>
2017-02-22 14:59:14 +0800

    添加了登录页面

---
+++ denlu.php
@@ -0,0 +1,19 @@
+<?php 
+var_dump(is_null($_POST));
+
+
+
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title></title>
+ </head>
+ <body>
+	 <form action="" method="post">
+	 	<input type="" name="">
+	 	<input type="submit">
+	 </form>
+ </body>
+ </html>
